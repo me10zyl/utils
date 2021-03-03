76ce7d377a6c530e79a13bb0578efa79d45adc15
astashz <me10zyl@qq.com>
2021-03-03 14:22:52 +0800

    update util

---
+++ bluesteel-core/src/main/java/com/yilnz/bluesteel/mockito/MockUtil.java
@@ -0,0 +1,50 @@
+package com.yilnz.bluesteel.mockito;
+
+import cn.hutool.core.util.RandomUtil;
+import com.yilnz.bluesteel.reflection.ReflectionUtil;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public class MockUtil {
+
+    public static String randomString(){
+        return RandomUtil.randomString(10);
+    }
+
+    public static int randomInt(){
+        return RandomUtil.randomInt();
+    }
+
+    public static BigDecimal randomBigDecimal() {
+        return RandomUtil.randomBigDecimal();
+    }
+
+    public static  <T> T mock(Class<T> type) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        try {
+            T t = type.getConstructor().newInstance();
+            List<Method> setterMethods = ReflectionUtil.getAllSetterMethods(type);
+            for (Method setterMethod : setterMethods) {
+                Class<?>[] parameterTypes = setterMethod.getParameterTypes();
+                if(parameterTypes[0].equals(String.class)){
+                    setterMethod.invoke(t, randomString());
+                }else if(parameterTypes[0].equals(Date.class)){
+                    setterMethod.invoke(t, new Date());
+                }else if(parameterTypes[0].equals(Integer.class)){
+                    setterMethod.invoke(t, randomInt());
+                }else if(parameterTypes[0].equals(BigDecimal.class)){
+                    setterMethod.invoke(t, randomBigDecimal());
+                }
+            }
+            return t;
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            //e.printStackTrace();
+            throw e;
+        }
+    }
+
+
+}
