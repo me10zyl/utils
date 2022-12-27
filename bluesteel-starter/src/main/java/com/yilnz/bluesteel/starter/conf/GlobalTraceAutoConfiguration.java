242c927698d4939702821c735e5e20af72a1df66
zyl-windows <yangguang>
2022-12-27 11:04:22 +0800

    globalTrace

--- bluesteel-starter/src/main/java/com/yilnz/bluesteel/starter/conf/GlobalTraceAutoConfiguration.java
+++ bluesteel-starter/src/main/java/com/yilnz/bluesteel/starter/conf/GlobalTraceAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.yilnz.bluesteel.framework.aop.HttpRequestAdvisor;
 import com.yilnz.bluesteel.framework.globaltrace.GlobalTraceFilter;
 import com.yilnz.bluesteel.starter.properties.GlobalTraceProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -15,12 +16,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties(GlobalTraceProperties.class)
 @RequiredArgsConstructor
+@Slf4j
 public class GlobalTraceAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(name = "globalTraceFilter")
     @ConditionalOnProperty(value = "globaltrace.trace-id.enable", havingValue = "true")
     public FilterRegistrationBean globalTraceFilter() {
+        log.info("enabled globalTraceId");
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new GlobalTraceFilter(), new ServletRegistrationBean[0]);
         filterRegistrationBean.setOrder(Integer.MIN_VALUE);
         return filterRegistrationBean;
@@ -30,6 +33,7 @@ public class GlobalTraceAutoConfiguration {
     @ConditionalOnMissingBean
     @ConditionalOnProperty(value = "globaltrace.http-request-aspect.enable")
     public HttpRequestAdvisor httpRequestAdvisor(GlobalTraceProperties properties){
+        log.info("enabled httpRequestAspect");
         return new HttpRequestAdvisor(properties.getHttpRequestAspect().getAopPattern());
     }
 
